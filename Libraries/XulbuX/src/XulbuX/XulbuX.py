0cc5fb27fd15d97b66ed5656cce55f8ae8787938
XulbuX <142539516+XulbuX-dev@users.noreply.github.com>
2024-10-27 15:20:23 +0100

    x

--- Libraries/XulbuX/src/XulbuX/XulbuX.py
+++ Libraries/XulbuX/src/XulbuX/XulbuX.py
@@ -1,24 +1,3 @@
-"""
- >>> import XulbuX as xx
-━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━
-  *  CUSTOM TYPES:
-      *  rgb(int,int,int,float)
-      *  hsl(int,int,int,float)
-      *  hexa(str)
-  *  PATH OPERATIONS          xx.Path
-  *  FILE OPERATIONS          xx.File
-  *  JSON FILE OPERATIONS     xx.Json
-  *  SYSTEM ACTIONS           xx.System
-  *  MANAGE ENVIRONMENT VARS  xx.EnvVars
-  *  CMD LOG AND ACTIONS      xx.Cmd
-  *  PRETTY PRINTING          xx.FormatCodes
-  *  COLOR OPERATIONS         xx.Color
-  *  DATA OPERATIONS          xx.Data
-  *  STR OPERATIONS           xx.String
-  *  CODE STRING OPERATIONS   xx.Code
-  *  REGEX PATTERN TEMPLATES  xx.Regex
-"""
-
 def check_libs(libs:list[str], install_missing:bool = False, confirm_install:bool = True) -> None|list[str]:
   missing = []
   for lib in libs:
