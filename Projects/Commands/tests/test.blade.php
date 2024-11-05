c4d6a0df5b91fe0b6a71aefcc13286efbd4a78ce
XulbuX <142539516+XulbuX-dev@users.noreply.github.com>
2024-11-05 12:07:18 +0100

    x

---
+++ Projects/Commands/tests/test.blade.php
@@ -0,0 +1,8 @@
+<x-guest.app class="px-0 py-3 sm:px-0 md:px-0 xl:p-6">
+    <x-slot name="title">
+        {{ __('l.results') }} {{ $race->name }}
+    </x-slot>
+
+    <iconify-icon icon="akar-icons:github-fill"></iconify-icon>
+
+</x-guest.app>
