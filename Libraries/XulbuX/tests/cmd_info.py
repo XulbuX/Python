d380f898a70ff7fb28309916d92cb769ff4af473
XulbuX <142539516+XulbuX-dev@users.noreply.github.com>
2024-11-08 16:49:46 +0100

    x

--- Libraries/XulbuX/tests/cmd_info.py
+++ Libraries/XulbuX/tests/cmd_info.py
@@ -1,8 +1,7 @@
 import XulbuX as xx
-import os
 
 
 if __name__ == '__main__':
     print(xx.Cmd.user())
     print(f'W: {xx.Cmd.w()}, H: {xx.Cmd.h()}')
-    print('#' * xx.Cmd.w(), flush=True)
+    print('#' * xx.Cmd.w())
