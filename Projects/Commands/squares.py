e928cd68789906aa185c7c1ad0b64a2fe51e3f74
XulbuX <j.buytenhuijs@gmail.com>
2025-10-07 09:36:54 +0200

    x

--- Projects/Commands/squares.py
+++ Projects/Commands/squares.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 """Get the squares of all numbers up to a given number."""
 from xulbux.base.consts import CHARS
-from xulbux import Console
+from xulbux import FormatCodes, Console
 import keyboard
 
 
@@ -21,47 +21,46 @@ def wait_key_pressed_and_released(key: str) -> None:
 
 
 def main():
-    while True:
-        print(
-            "═══════════════════ SQUARED NUMBERS - ALL OF THEM! ═══════════════════\n"
-            ">> hold SPACE to pause, while the program is writing down the numbers\n"
-            "━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━"
-        )
+    FormatCodes.print(
+        "═══════════════════ [b](SQUARED NUMBERS - ALL OF THEM!) ═══════════════════\n"
+        ">> hold SPACE to pause, while the program is writing down the numbers\n"
+        "━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━"
+    )
 
-        loops = int(
-            Console.input(
-                "Until which number do you want all squares to be calculated: ",
-                allowed_chars=CHARS.DIGITS,
-                min_len=1,
-                max_len=7,
-            ) or ""
-        )
+    loops = int(
+        Console.input(
+            "Until which number do you want all squares to be calculated: ",
+            allowed_chars=CHARS.DIGITS,
+            min_len=1,
+            max_len=7,
+        ) or ""
+    )
 
-        i = 1
-        row_space = len(f"│ {loops}² = {loops * loops:,} │")
+    i = 1
+    row_space = len(f"│ {loops}² = {loops * loops:,} │")
 
-        borders = {
-            "top": ("╭" + ((row_space * TABLE_COLS) - 2) * "─" + "╮"),
-            "bottom": ("╰" + ((row_space * TABLE_COLS) - 2) * "─" + "╯")
-        }
+    borders = {
+        "top": ("╭" + ((row_space * TABLE_COLS) - 2) * "─" + "╮"),
+        "bottom": ("╰" + ((row_space * TABLE_COLS) - 2) * "─" + "╯")
+    }
 
-        print(borders["top"])
-        while i <= loops:
-            row = ""
-            if keyboard.is_pressed("space"):
-                wait_key_pressed_and_released("space")
-            for _ in range(TABLE_COLS):
-                if i <= loops:
-                    output = f"│ {i}² = {i * i:,}"
-                    row += f"{output}{(row_space - len(output) - 1) * " "}│"
-                else:
-                    row += f"│{(row_space - 2) * " "}│"
-                i += 1
-            print(row)
-        print(borders["bottom"])
-        print()
+    print(borders["top"])
+    while i <= loops:
+        row = ""
+        if keyboard.is_pressed("space"):
+            wait_key_pressed_and_released("space")
+        for _ in range(TABLE_COLS):
+            if i <= loops:
+                output = f"│ {i}² = {i * i:,}"
+                row += f"{output}{(row_space - len(output) - 1) * " "}│"
+            else:
+                row += f"│{(row_space - 2) * " "}│"
+            i += 1
+        print(row)
+    print(borders["bottom"])
+    print()
 
-        Console.cls()
+    Console.cls()
 
 
 if __name__ == "__main__":
